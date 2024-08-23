6d49392ac40015b292dddc2748b14736897b37a8
link <>
2024-08-23 10:14:54 -0600

    First Commit

---
+++ Etiquetas UCC/AplicacionEtiquetaUCC.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AplicacionEtiquetaUCC
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNoPedido = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnImprimirTodo = New System.Windows.Forms.Button()
+        Me.btnImprimiSeleccionado = New System.Windows.Forms.Button()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.carton = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Proveedor = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Sucursal = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.oc = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Factura = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Piezas = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CodigodeBarras = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.notienda = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.razonsocial = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.peso = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "No de Pedido:"
+        '
+        'txtNoPedido
+        '
+        Me.txtNoPedido.Location = New System.Drawing.Point(93, 16)
+        Me.txtNoPedido.Name = "txtNoPedido"
+        Me.txtNoPedido.Size = New System.Drawing.Size(100, 20)
+        Me.txtNoPedido.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(199, 14)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Consultar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnImprimirTodo
+        '
+        Me.btnImprimirTodo.Location = New System.Drawing.Point(15, 181)
+        Me.btnImprimirTodo.Name = "btnImprimirTodo"
+        Me.btnImprimirTodo.Size = New System.Drawing.Size(125, 23)
+        Me.btnImprimirTodo.TabIndex = 3
+        Me.btnImprimirTodo.Text = "Imprimir Todo"
+        Me.btnImprimirTodo.UseVisualStyleBackColor = True
+        '
+        'btnImprimiSeleccionado
+        '
+        Me.btnImprimiSeleccionado.Location = New System.Drawing.Point(149, 181)
+        Me.btnImprimiSeleccionado.Name = "btnImprimiSeleccionado"
+        Me.btnImprimiSeleccionado.Size = New System.Drawing.Size(125, 23)
+        Me.btnImprimiSeleccionado.TabIndex = 4
+        Me.btnImprimiSeleccionado.Text = "Imprimir Seleccionado"
+        Me.btnImprimiSeleccionado.UseVisualStyleBackColor = True
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Location = New System.Drawing.Point(720, 181)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCerrar.TabIndex = 5
+        Me.btnCerrar.Text = "Cerrar"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.carton, Me.Proveedor, Me.Sucursal, Me.oc, Me.Factura, Me.Piezas, Me.CodigodeBarras, Me.notienda, Me.razonsocial, Me.peso})
+        Me.DataGridView1.Location = New System.Drawing.Point(15, 42)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(780, 133)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'carton
+        '
+        Me.carton.DataPropertyName = "carton"
+        Me.carton.HeaderText = "Cartón"
+        Me.carton.Name = "carton"
+        '
+        'Proveedor
+        '
+        Me.Proveedor.DataPropertyName = "proveedor"
+        Me.Proveedor.HeaderText = "proveedor"
+        Me.Proveedor.Name = "Proveedor"
+        Me.Proveedor.Visible = False
+        '
+        'Sucursal
+        '
+        Me.Sucursal.DataPropertyName = "sucursal"
+        Me.Sucursal.HeaderText = "Sucursal"
+        Me.Sucursal.Name = "Sucursal"
+        Me.Sucursal.Width = 225
+        '
+        'oc
+        '
+        Me.oc.DataPropertyName = "oc"
+        Me.oc.HeaderText = "O.C."
+        Me.oc.Name = "oc"
+        '
+        'Factura
+        '
+        Me.Factura.DataPropertyName = "factura"
+        Me.Factura.HeaderText = "Factura"
+        Me.Factura.Name = "Factura"
+        '
+        'Piezas
+        '
+        Me.Piezas.DataPropertyName = "piezas"
+        Me.Piezas.HeaderText = "Piezas"
+        Me.Piezas.Name = "Piezas"
+        '
+        'CodigodeBarras
+        '
+        Me.CodigodeBarras.DataPropertyName = "codigobarras"
+        Me.CodigodeBarras.HeaderText = "Código de Barras"
+        Me.CodigodeBarras.Name = "CodigodeBarras"
+        '
+        'notienda
+        '
+        Me.notienda.DataPropertyName = "notienda"
+        Me.notienda.HeaderText = "notienda"
+        Me.notienda.Name = "notienda"
+        Me.notienda.Visible = False
+        '
+        'razonsocial
+        '
+        Me.razonsocial.DataPropertyName = "razonsocial"
+        Me.razonsocial.HeaderText = "razonsocial"
+        Me.razonsocial.Name = "razonsocial"
+        Me.razonsocial.Visible = False
+        '
+        'peso
+        '
+        Me.peso.DataPropertyName = "peso"
+        Me.peso.HeaderText = "peso"
+        Me.peso.Name = "peso"
+        Me.peso.Visible = False
+        '
+        'AplicacionEtiquetaUCC
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(807, 215)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.Controls.Add(Me.btnImprimiSeleccionado)
+        Me.Controls.Add(Me.btnImprimirTodo)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtNoPedido)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "AplicacionEtiquetaUCC"
+        Me.Text = "Aplicación para Etiqueta UCC"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNoPedido As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnImprimirTodo As Button
+    Friend WithEvents btnImprimiSeleccionado As Button
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents carton As DataGridViewTextBoxColumn
+    Friend WithEvents Proveedor As DataGridViewTextBoxColumn
+    Friend WithEvents Sucursal As DataGridViewTextBoxColumn
+    Friend WithEvents oc As DataGridViewTextBoxColumn
+    Friend WithEvents Factura As DataGridViewTextBoxColumn
+    Friend WithEvents Piezas As DataGridViewTextBoxColumn
+    Friend WithEvents CodigodeBarras As DataGridViewTextBoxColumn
+    Friend WithEvents notienda As DataGridViewTextBoxColumn
+    Friend WithEvents razonsocial As DataGridViewTextBoxColumn
+    Friend WithEvents peso As DataGridViewTextBoxColumn
+End Class
---
+++ Etiquetas UCC/RawPrinter.vb
@@ -0,0 +1,158 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.IO
+
+Public Class RawPrinter
+    ' Structure and API declarions:
+    <StructLayout(LayoutKind.Sequential, CharSet:=CharSet.Ansi)>
+    Public Class DOCINFOA
+        <MarshalAs(UnmanagedType.LPStr)>
+        Public pDocName As String
+        <MarshalAs(UnmanagedType.LPStr)>
+        Public pOutputFile As String
+        <MarshalAs(UnmanagedType.LPStr)>
+        Public pDataType As String
+    End Class
+    <DllImport("winspool.Drv", EntryPoint:="OpenPrinterA", SetLastError:=True, CharSet:=CharSet.Ansi, ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function OpenPrinter(<MarshalAs(UnmanagedType.LPStr)> szPrinter As String, ByRef hPrinter As IntPtr, pd As IntPtr) As Boolean
+    End Function
+
+    <DllImport("winspool.Drv", EntryPoint:="ClosePrinter", SetLastError:=True, ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function ClosePrinter(hPrinter As IntPtr) As Boolean
+    End Function
+
+    <DllImport("winspool.Drv", EntryPoint:="StartDocPrinterA", SetLastError:=True, CharSet:=CharSet.Ansi, ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function StartDocPrinter(hPrinter As IntPtr, level As Int32, <[In], MarshalAs(UnmanagedType.LPStruct)> di As DOCINFOA) As Boolean
+    End Function
+
+    <DllImport("winspool.Drv", EntryPoint:="EndDocPrinter", SetLastError:=True, ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function EndDocPrinter(hPrinter As IntPtr) As Boolean
+    End Function
+
+    <DllImport("winspool.Drv", EntryPoint:="StartPagePrinter", SetLastError:=True, ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function StartPagePrinter(hPrinter As IntPtr) As Boolean
+    End Function
+
+    <DllImport("winspool.Drv", EntryPoint:="EndPagePrinter", SetLastError:=True, ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function EndPagePrinter(hPrinter As IntPtr) As Boolean
+    End Function
+
+    <DllImport("winspool.Drv", EntryPoint:="WritePrinter", SetLastError:=True, ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function WritePrinter(hPrinter As IntPtr, pBytes As IntPtr, dwCount As Int32, ByRef dwWritten As Int32) As Boolean
+    End Function
+
+    ' SendBytesToPrinter()
+    ' When the function is given a printer name and an unmanaged array
+    ' of bytes, the function sends those bytes to the print queue.
+    ' Returns true on success, false on failure.
+    Public Shared Function SendBytesToPrinter(szPrinterName As String, pBytes As IntPtr, dwCount As Int32) As Boolean
+        Dim dwError As Int32 = 0, dwWritten As Int32 = 0
+        Dim hPrinter As New IntPtr(0)
+        Dim di As New DOCINFOA()
+        Dim bSuccess As Boolean = False
+        ' Assume failure unless you specifically succeed.
+        di.pDocName = "My C#.NET RAW Document"
+        di.pDataType = "RAW"
+
+        ' Open the printer.
+        If OpenPrinter(szPrinterName.Normalize(), hPrinter, IntPtr.Zero) Then
+            ' Start a document.
+            If StartDocPrinter(hPrinter, 1, di) Then
+                ' Start a page.
+                If StartPagePrinter(hPrinter) Then
+                    ' Write your bytes.
+                    bSuccess = WritePrinter(hPrinter, pBytes, dwCount, dwWritten)
+                    EndPagePrinter(hPrinter)
+                End If
+                EndDocPrinter(hPrinter)
+            End If
+            ClosePrinter(hPrinter)
+        End If
+        ' If you did not succeed, GetLastError may give more information
+        ' about why not.
+        If bSuccess = False Then
+            dwError = Marshal.GetLastWin32Error()
+        End If
+        Return bSuccess
+    End Function
+
+    Public Shared Function SendFileToPrinter(szPrinterName As String, szFileName As String) As Boolean
+        ' Open the file.
+        Dim fs As New FileStream(szFileName, FileMode.Open)
+        ' Create a BinaryReader on the file.
+        Dim br As New BinaryReader(fs)
+        ' Dim an array of bytes big enough to hold the file's contents.
+        Dim bytes As [Byte]() = New [Byte](fs.Length - 1) {}
+        Dim bSuccess As Boolean = False
+        ' Your unmanaged pointer.
+        Dim pUnmanagedBytes As New IntPtr(0)
+        Dim nLength As Integer
+
+        nLength = Convert.ToInt32(fs.Length)
+        ' Read the contents of the file into the array.
+        bytes = br.ReadBytes(nLength)
+        ' Allocate some unmanaged memory for those bytes.
+        pUnmanagedBytes = Marshal.AllocCoTaskMem(nLength)
+        ' Copy the managed byte array into the unmanaged array.
+        Marshal.Copy(bytes, 0, pUnmanagedBytes, nLength)
+        ' Send the unmanaged bytes to the printer.
+        bSuccess = SendBytesToPrinter(szPrinterName, pUnmanagedBytes, nLength)
+        ' Free the unmanaged memory that you allocated earlier.
+        Marshal.FreeCoTaskMem(pUnmanagedBytes)
+        Return bSuccess
+    End Function
+
+    Public Shared Function SendStringToPrinter(szPrinterName As String, szString As String) As Boolean
+        Dim pBytes As IntPtr
+        Dim dwCount As Int32
+        ' How many characters are in the string?
+        dwCount = szString.Length
+        ' Assume that the printer is expecting ANSI text, and then convert
+        ' the string to ANSI text.
+        pBytes = Marshal.StringToCoTaskMemAnsi(szString)
+        ' Send the converted ANSI string to the printer.
+        SendBytesToPrinter(szPrinterName, pBytes, dwCount)
+        Marshal.FreeCoTaskMem(pBytes)
+        Return True
+    End Function
+
+    Public Shared Function SendBytesToPrinterPunteros(ByVal printerName As String, ByVal data() As Byte) As Boolean
+        Dim dwError As Integer = 0
+        Dim dwWritten As Integer = 0
+        Dim hPrinter As IntPtr = IntPtr.Zero
+        Dim di As New DOCINFOA()
+        Dim bSuccess As Boolean = False
+
+        di.pDocName = "ESC/POS Document"
+        di.pDataType = "RAW"
+
+        ' Abrir la impresora.
+        If OpenPrinter(printerName, hPrinter, IntPtr.Zero) Then
+            ' Iniciar un documento.
+            If StartDocPrinter(hPrinter, 1, di) Then
+                ' Iniciar una página.
+                If StartPagePrinter(hPrinter) Then
+                    ' Crear un puntero para los datos de impresión
+                    Dim pBytes As IntPtr = Marshal.AllocHGlobal(data.Length)
+                    Marshal.Copy(data, 0, pBytes, data.Length)
+
+                    ' Enviar los datos a la impresora.
+                    bSuccess = WritePrinter(hPrinter, pBytes, data.Length, dwWritten)
+
+                    ' Liberar el puntero de memoria
+                    Marshal.FreeHGlobal(pBytes)
+
+                    EndPagePrinter(hPrinter)
+                End If
+                EndDocPrinter(hPrinter)
+            End If
+            ClosePrinter(hPrinter)
+        End If
+
+        If Not bSuccess Then
+            dwError = Marshal.GetLastWin32Error()
+        End If
+
+        Return bSuccess
+    End Function
+
+End Class
